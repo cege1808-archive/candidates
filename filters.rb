2fc87c11f484ee1c1be61a0981e04e9755e85ec2
cege1808 <gunawan.clarissa@hotmail.com>
2016-06-27 03:27:45 +0000

    add features

--- filters.rb
+++ filters.rb
@@ -2,10 +2,48 @@
 # This way, we keep these methods separated from other potential parts of the program
 
 def find(id)
-
+  candidate = nil
+  @candidates.each do |hash|
+    if hash[:id] == id
+      candidate = hash
+    end
+  end
+  candidate
 end
+
 
 def experienced?(candidate)
+  candidate[:years_of_experience] > 1
 end
 
-# More methods will go below
+def github?(candidate)
+  candidate[:github_points] >= 100
+end
+
+def age?(candidate)
+  candidate[:age] > 18
+end
+
+def applied_date?(candidate)
+  candidate[:date_applied] >= 15.days.ago.to_date
+end
+
+def rubyorpython?(candidate)
+  candidate[:languages].include? ('Ruby' || "Python")
+end
+
+def qualified_candidates
+  qualified=[]
+  @candidates.each do |candidate|
+    if experienced?(candidate) && github?(candidate) && age?(candidate) && applied_date?(candidate) && rubyorpython?(candidate) 
+    qualified << candidate
+    end
+  end
+  qualified
+end
+
+def ordered_by_qualifications
+  qualified = @candidates.sort_by do |candidate| 
+    [-candidate[:years_of_experience], -candidate[:github_points]]
+  end
+end
--- main.rb
+++ main.rb
@@ -6,7 +6,24 @@ require './candidates'
 require './filters'
 
 ## Your test code can go here
+# puts experienced?(@candidates[0])
+# pp find(7)
+# pp qualified_candidates
+# p rubyorpython?(@candidates[2])
+# pp ordered_by_qualifications
 
-# binding.pry
-
-pp qualified_candidates
+stay = true
+while stay
+  puts "Find/All/Qualified Candidates"
+  user_response = gets.chomp.downcase
+  if user_response =~ /find/
+    pp find(/(\d+)/.match(user_response).to_s.to_i)
+  elsif user_response =~/all/
+    pp @candidates
+  elsif user_response =~ /qualified/
+    pp qualified_candidates
+  elsif user_response =~ /quit/
+    stay = false
+  end
+end
+    
