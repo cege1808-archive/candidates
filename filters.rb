f520d9df52ffcaeb8df0468da12c5a52dd1b5b7a
cege1808 <gunawan.clarissa@hotmail.com>
2016-06-27 21:19:40 +0000

    add exceptions

--- filters.rb
+++ filters.rb
@@ -1,7 +1,12 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
 
+class InvalidCandidateError < StandardError
+end
+
+
 def find(id)
+  raise '@candidates must be an Array' if @candidates.nil?
   candidate = nil
   @candidates.each do |hash|
     if hash[:id] == id
@@ -13,14 +18,24 @@ end
 
 
 def experienced?(candidate)
-  candidate[:years_of_experience] > 1
+  unless candidate.has_key?(:years_of_experience)
+    raise InvalidCandidateError, 'candidate must have a :years of experience key'
+  end
+  candidate[:years_of_experience] >= 2 
 end
 
+
 def github?(candidate)
+  unless candidate[:github_points].is_a? Integer
+    raise InvalidCandidateError, 'candidate\'s  gitpoints must be an integer'
+  end
   candidate[:github_points] >= 100
 end
 
 def age?(candidate)
+  unless (candidate[:age].is_a? Integer) || (candidate[:age].is_a? Float)
+    raise InvalidCandidateError, 'candidate\'s age must be an integer or float'
+  end
   candidate[:age] > 18
 end
 
--- main.rb
+++ main.rb
@@ -12,18 +12,62 @@ require './filters'
 # p rubyorpython?(@candidates[2])
 # pp ordered_by_qualifications
 
-stay = true
-while stay
-  puts "Find/All/Qualified Candidates"
-  user_response = gets.chomp.downcase
-  if user_response =~ /find/
-    pp find(/(\d+)/.match(user_response).to_s.to_i)
-  elsif user_response =~/all/
-    pp @candidates
-  elsif user_response =~ /qualified/
-    pp qualified_candidates
-  elsif user_response =~ /quit/
-    stay = false
+candidate = {
+    id: 5,
+    # years_of_experience: 4,
+    github_points: "cat", #293,
+    languages: ['C', 'Ruby', 'Python', 'Clojure'],
+    date_applied: 5.days.ago.to_date,
+    age: 26.5 #26
+  }
+
+#Exceptions
+  begin 
+    if github?(candidate)
+      puts "Candidate #{candidate[:id]} has a good amount of github points"
+    else
+      puts "Candidate #{candidate[:id]} does not have a good amount of github points"
+    end
+  rescue InvalidCandidateError => ex
+    puts "It could not be determined whether the candidate has a good amount of github points"
+    puts "The reason was: #{ex. message}"
   end
-end
+
+  begin 
+    if age?(candidate)
+      puts "Candidate #{candidate[:id]} is at a working age"
+    else
+      puts "Candidate #{candidate[:id]} is not at a working age"
+    end
+  rescue InvalidCandidateError => ex
+    puts "It could not be determined whether the candidate is at a working age"
+    puts "The reason was: #{ex. message}"
+  end
+
+  begin 
+    if experienced?(candidate)
+      puts "Candidate #{candidate[:id]} is experienced enough"
+    else
+      puts "Candidate #{candidate[:id]} is not experienced enough"
+    end
+  rescue InvalidCandidateError => ex
+    puts "It could not be determined whether the candidate is experienced enough"
+    puts "The reason was: #{ex. message}"
+  end
+
+
+# stay = true
+# while stay
+#   puts "Find/All/Qualified Candidates"
+#   user_response = gets.chomp.downcase
+#   if user_response =~ /find/
+#     pp find(/(\d+)/.match(user_response).to_s.to_i)
+#   elsif user_response =~/all/
+#     pp @candidates
+#   elsif user_response =~ /qualified/
+#     pp qualified_candidates
+#   elsif user_response =~ /quit/
+#     stay = false
+#   end
+# end
     
